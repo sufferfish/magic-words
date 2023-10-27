c528396df52db5ea5d81df1bd87e6a2f7a0985de
Nate <flyingwithnate@pm.me>
2023-10-26 19:55:41 -0700

    Start handling win condition
    yeet

--- gamejs/data.mjs
+++ gamejs/data.mjs
@@ -2,12 +2,14 @@ import { createClient } from '@supabase/supabase-js';
 import 'dotenv/config'
 
 const supabase = createClient(process.env.api, process.env.Ckey)
-const { word, error } = await supabase
+const { data, error } = await supabase
     .from('gamewords')
-    .select('*')
+    .select('id, word')
+    .eq('is_guessed', 'false')
+    .order('created_at', { ascending: true })
 
 if (error == null) {
-    console.log(`Retrieved word: ${word}`);
+    console.log(data)
 } else {
     console.log(error);
 }
--- gamejs/index.js
+++ gamejs/index.js
@@ -9,29 +9,44 @@ async function getWord() {
     }
 
     const supabase = supa.createClient(process.env.api, process.env.cKey);
-    const { word, error } = await supabase
-        .from('gamewords')
-        .select('word')
+    const { data, error } = await supabase
+    .from('gamewords')
+    .select('id, word')
+    .eq('is_guessed', 'false')
+    .order('created_at', { ascending: true });
+
+    let word = data[0]['word']
+    let id = data[0]['id']
 
     if (error == null) {
         console.log(`Retrieved word: ${word}`);
-        return word;
+        return [word, id];
     } else {
         console.log(error);
     }
 }
 
+async function postWinner(winner, id) {
+    const supabase = supa.createClient(process.env.api, process.env.cKey);
+    const { error } = await supabase
+    .from('gamewords')
+    .update({is_guessed: 'true', winner: winner})
+    .eq('id', id)
+
+    // TODO: handle the errors
+}
+
 functions.http('gameHandler', (req, res) => {
     let g = req.query.word;
-    let w = getWord();
+    let p = req.query.player;
+    let wordandid = getWord();
 
-    console.log(w);
-
-    if (g === w) {
-        res.send(`You guessed the word! It was ${g}.`);
-    } else if (g === undefined) {
+    if (g === undefined) {
         res.send("This is a test mode.")
+    } else if (g != undefined && g.toLowerCase() === wordandid[0]) {
+        postWinner(p, wordandid[1])
+        res.send(`You guessed the word ${p}! It was ${g}.`);
     } else {
-        res.send(`Sorry. The word ${g} is not correct.`)
+        res.send(`Sorry ${p}, the word ${g} is not correct.`)
     }
 });

3eebd57e7f68467387c6063ee2b11486075c2a68
Nate <flyingwithnate@pm.me>
2023-10-27 20:23:18 -0700

    Pulls winner
    Roughly working right now. Need to get the win condition working.

--- gamejs/data.mjs
+++ gamejs/data.mjs
@@ -4,12 +4,13 @@ import 'dotenv/config'
 const supabase = createClient(process.env.api, process.env.Ckey)
 const { data, error } = await supabase
     .from('gamewords')
-    .select('id, word')
+    .select('id, word, winner')
     .eq('is_guessed', 'false')
     .order('created_at', { ascending: true })
 
+let info = data[0];
 if (error == null) {
-    console.log(data)
+    console.log(info)
 } else {
     console.log(error);
 }
--- gamejs/index.js
+++ gamejs/index.js
@@ -11,16 +11,17 @@ async function getWord() {
     const supabase = supa.createClient(process.env.api, process.env.cKey);
     const { data, error } = await supabase
     .from('gamewords')
-    .select('id, word')
+    .select('id, word, winner')
     .eq('is_guessed', 'false')
     .order('created_at', { ascending: true });
 
     let word = data[0]['word']
     let id = data[0]['id']
+    let winner = data[0]['winner']
 
     if (error == null) {
         console.log(`Retrieved word: ${word}`);
-        return [word, id];
+        return [word, id, winner];
     } else {
         console.log(error);
     }
@@ -34,12 +35,19 @@ async function postWinner(winner, id) {
     .eq('id', id)
 
     // TODO: handle the errors
+    if (error != null) {
+        console.log(`Failed to post winner for id: ${id}`);
+    } else {
+        console.log(`Succeeded in posting winner for id: ${id}`);
+    }
 }
 
 functions.http('gameHandler', (req, res) => {
     let g = req.query.word;
     let p = req.query.player;
     let wordandid = getWord();
+
+    console.log(wordandid);
 
     if (g === undefined) {
         res.send("This is a test mode.")

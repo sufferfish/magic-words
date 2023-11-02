4ff51b2c49ee84405d9776bcb7ee5e44d141f884
Nate <flyingwithnate@pm.me>
2023-11-01 21:11:36 -0700

    New word functionality done

--- tooling/index.js
+++ tooling/index.js
@@ -3,23 +3,46 @@ const supabase = require('@supabase/supabase-js');
 require('dotenv').config();
 
 async function postWord(word, hint, active) {
-    const sb = createClient(process.env.URL, process.env.SERVICE);
+    const sb = supabase.createClient(process.env.URL, process.env.SERVICE);
 
     const { error } = await sb
         .from('gamewords')
         .insert([
-            { word: word, hint: hint, active: active},
+            { word: word, hint: hint, active: active },
         ]);
-    
-    if (error === null) {
+
+    console.log(error);
+    if (error !== null) {
         return true;
     } else {
         return false;
     }
 }
 
-functions.http('newword', async (req, res) => {
+functions.http('newWord', async (req, res) => {
     let word = req.body.word;
     let hint = req.body.hint;
     let active = req.body.active;
+
+    if (req.headers.authorization === process.env.tKey) {
+
+        if (word === undefined | hint === undefined | active === undefined) {
+            res.sendStatus(400);
+        } else {
+            console.log(`Received ${word} and "${hint}" and word status: ${active}.`);
+
+            let err = postWord(word, hint, active);
+
+            if (!err) {
+                console.log(`Failed to post ${word} with hint to database.`);
+                console.log(err);
+                res.sendStatus(400);
+            } else {
+                console.log(`Posted ${word} to database.`);
+                res.sendStatus(200);
+            }
+        }
+    } else {
+        res.sendStatus(401);
+    };
 });
